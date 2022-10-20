53fe36a245b6e32e3b0acc6be53017b007bd354d
dieego14 <dieegohernandez14@gmail.com>
2022-10-19 22:45:25 -0700

    Initialized pseudocode

---
+++ midterm.cpp
@@ -0,0 +1,14 @@
+// Programmer: Diego
+//
+// Purpose: Create a program that calculates the gross and 
+//          net box office revenue for a night
+//
+// Start
+//
+//      Input: Display
+//
+//      Programming:
+//
+//      Output:
+//
+// Stop
