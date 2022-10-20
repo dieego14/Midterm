bb28b4adc883669a21042ce5561b7f9ed1bc01a0
dieego14 <dieegohernandez14@gmail.com>
2022-10-19 22:50:26 -0700

    updated pseudocode

--- midterm.cpp
+++ midterm.cpp
@@ -5,10 +5,17 @@
 //
 // Start
 //
-//      Input: Display
+//      Input: Prompt user to enter the name of movie
+//             Wait for user to enter the name of the movie
+//             Prompt user to enter the number of adult tickets sold
+//             Wait for user to enter the number of adult tickets sold
+//             Prompt user to enter the number of child tickets sold
+//             Wait for user to enter the number of child tickets sold
 //
-//      Programming:
+//      Programming: 
+//             
 //
-//      Output:
+//      Output: Display the name of the movie input by the user, the number of adult & child tickets sold,
+//              gross box office revenue, amount paid to distributer, and net box office revenue 
 //
 // Stop
