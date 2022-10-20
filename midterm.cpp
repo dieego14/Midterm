f619b833956de7bfc64d4c9071b794ef1ec763bd
dieego14 <dieegohernandez14@gmail.com>
2022-10-19 23:28:54 -0700

    First submission

--- midterm.cpp
+++ midterm.cpp
@@ -1,6 +1,6 @@
 // Programmer: Diego
 //
-// Purpose: Create a program that calculates the gross and 
+// Purpose: Create a program that calculates the gross and
 //          net box office revenue for a night
 //
 // Start
@@ -12,11 +12,11 @@
 //             Prompt user to enter the number of child tickets sold
 //             Wait for user to enter the number of child tickets sold
 //
-//      Programming: 
-//             
+//      Programming:
+//
 //
 //      Output: Display the name of the movie input by the user, the number of adult & child tickets sold,
-//              gross box office revenue, amount paid to distributer, and net box office revenue 
+//              gross box office revenue, amount paid to distributer, and net box office revenue
 //
 // Stop
 
@@ -25,33 +25,38 @@
 #include <iomanip>
 using namespace std;
 
-int main(){
+int main()
+{
     const double ADULT_TICKET_PRICE = 10.00;
-    int adultTicketsSold;
     const double CHILD_TICKET_PRICE = 6.00;
+    const double DISTRIBUTER_CUT = 0.20;
+    const double MOVIE_THEATER_CUT = 0.80;
+    int adultTicketsSold;
     int childTicketsSold;
+    double grossBoxOffice;
+    double grossOfficeReveue;
+    double amountPaidToDistributer;
+    double netBoxOffice;
     string movieName;
 
-
-
-
     cout << "Enter movie name: ";
-    getline(cin,movieName);
+    getline(cin, movieName);
     cout << "Adult ticket sales: ";
     cin >> adultTicketsSold;
-    cout <<"Child ticket sales: ";
+    cout << "Child ticket sales: ";
     cin >> childTicketsSold;
-    cout <<endl<<endl;
-    
-    cout << "------------------------------------------";
+    cout << "----------------------------------------------------\n";
+    grossBoxOffice = (adultTicketsSold * ADULT_TICKET_PRICE) + (childTicketsSold * CHILD_TICKET_PRICE);
+    amountPaidToDistributer = (grossBoxOffice * DISTRIBUTER_CUT);
+    netBoxOffice = (grossBoxOffice * MOVIE_THEATER_CUT);
 
-    cout<<"Movie name: ";
-    cout<<"Adult Tickets Sold";
+    cout << fixed << setprecision(2);
+    cout << "Movie name:" << right << setw(26) << "\"" << movieName << "\"" << endl;
+    cout << "Adult Tickets Sold:" << right << setw(20) << adultTicketsSold << endl;
+    cout << "Child Tickets Sold:" << right << setw(20) << childTicketsSold << endl;
+    cout << "Gross Box Office Revenue:" << right << setw(13) << "$ " << grossBoxOffice << endl;
+    cout << "Amount Paid to Distributer:" << right << setw(11) << "-$ " << setw(7) << amountPaidToDistributer << endl;
+    cout << "Net Box Office Revenue:" << right << setw(15) << "$ " << netBoxOffice << endl;
 
-
-
-
-
-
-
+    return 0;
 }
